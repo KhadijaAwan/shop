3b543732eda30e8bdfeb6d44a95d33415cf41367
Khadija Awan <101884505+KhadijaAwan@users.noreply.github.com>
2023-11-14 01:04:32 +0500

    Update page.tsx

--- src/app/pages/productInfo/details/page.tsx
+++ src/app/pages/productInfo/details/page.tsx
@@ -3,8 +3,7 @@ import { getProductId } from "@/services/product/id_product";
 export const dynamic = "force-dynamic";
 
 export default async function ProductInfo({ params }: any) {
+  const product_Detail = await getProductId(params.details);
+  console.log(product_Detail);
+  return <ProductDetails productData={product_Detail && product_Detail.data} />;
 }
-//   const product_Detail = await getProductId(params.details);
-//   console.log(product_Detail);
-//   return <ProductDetails productData={product_Detail && product_Detail.data} />;
-// }
