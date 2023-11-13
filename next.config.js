9f8fa2732793257cd414dd8f1d5ed0921a101de9
Khadija Awan <101884505+KhadijaAwan@users.noreply.github.com>
2023-11-13 23:21:15 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     output: 'export',
-//     images: {
-//       loader: 'default',
-//     },
-//   }
-
-//   module.exports = nextConfig
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    images: {
+      loader: 'default',
+    },
+  }
 
-module.exports = nextConfig;
+  module.exports = nextConfig
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// module.exports = nextConfig;
