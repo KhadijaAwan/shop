17653d892da50a645c397e4d4f2c15a27dcb10c8
Khadija Awan <101884505+KhadijaAwan@users.noreply.github.com>
2023-11-13 18:35:36 +0500

    Update page.tsx

--- src/app/pages/men/page.tsx
+++ src/app/pages/men/page.tsx
@@ -1,14 +1,13 @@
 import CardContent from "@/components/cardContent";
 import { gettingCategory } from "../../../services/product/category_product/index";
 
-export const getMenProducts = async () => {
-  const clientProducts = await gettingCategory("men");
-  return clientProducts;
+const getProducts = async () => {
+  const response = await gettingCategory("men");
+  const getData = response.data;
+  return getData;
 };
 
-const Men = async () => {
-  const client = await getMenProducts();
-  return <CardContent data={client && client.data} />;
-};
-
-export default Men;
+export default async function Men() {
+  const clientProducts = await getProducts();
+  return <CardContent data={clientProducts} />;
+}
