a2450ffce60b0a47691b65ff1c6d31bf19f6c48a
Khadija Awan <101884505+KhadijaAwan@users.noreply.github.com>
2023-11-13 19:25:11 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,35 +8,18 @@
 
 //   module.exports = nextConfig
 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    loader: 'default',
+  },
+  target: 'experimental-serverless-trace',
+};
+
+module.exports = nextConfig;
+
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {};
 
 // module.exports = nextConfig;
-
-// next.config.js
-
-const commonConfig = {
-  images: {
-    loader: 'default',
-  },
-};
-
-const isApiRoute = (phase, { defaultConfig }) => {
-  return phase === 'api-handler';
-};
-
-const getOutputConfig = (isApi) => {
-  return isApi ? {} : { output: 'export' };
-};
-
-module.exports = (phase, { defaultConfig }) => {
-  const isApi = isApiRoute(phase, { defaultConfig });
-  const outputConfig = getOutputConfig(isApi);
-
-  return {
-    ...defaultConfig,
-    ...commonConfig,
-    ...outputConfig,
-  };
-};
-
