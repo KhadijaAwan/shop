928304ecff56ea48d6098ff6d7add32eeab05da9
Khadija Awan <101884505+KhadijaAwan@users.noreply.github.com>
2023-11-13 23:33:04 +0500

    Update page.tsx

--- src/app/pages/women/page.tsx
+++ src/app/pages/women/page.tsx
@@ -1,13 +1,7 @@
 import CardContent from "@/components/cardContent";
 import { gettingCategory } from "../../../services/product/category_product/index";
-
-const getProducts = async () => {
-  const response = await gettingCategory("women");
-  const getData = response.data;
-  return getData;
-};
-
+export const dynamic = "force-dynamic";
 export default async function Women() {
-  const clientProducts = await getProducts();
-  return <CardContent data={clientProducts} />;
+  const clientProducts = await gettingCategory("women");
+  return <CardContent data={clientProducts && clientProducts.data} />;
 }
